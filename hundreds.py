191b2b3d91b2b52fcd673915ff26fcd8b9f7df64
Michael Lampard <mlampa01@dcs.bbk.ac.uk>
2017-12-10 01:14:24 +0000

    Added main function

--- hundreds.py
+++ hundreds.py
@@ -1,9 +1,5 @@
 import random
 
-# Global variables
-computer_score = 0
-human_score = 0
-turn_total = 0
 
 def roll():
     """A function to generate the random number when a dice is rolled"""
@@ -13,25 +9,37 @@ def roll():
 
 
 
-#def human_move(computer_score, human_score):
-
-
-while roll != 1:
+def human_move(computer_score, human_score):
+    #turn variables
+    turn_total = 0 # Initialize turn total variable for function
+    finished = False
     # Constants
     roll_again = ('y', 'Y')
-    # Turn variables
+
+    while not finished:
 
 
-    print("Computers Score =  " + str(computer_score) + "\n")
-    print("Human Score =  " + str(human_score))
-    difference = human_score - computer_score
-    print("The scores difference is " + str(difference))
 
-    print("Would you like to roll?", end='' )
-    user_choice = input()
-    if user_choice in roll_again:
+        print("Computers Score =  " + str(computer_score) + "\n")
+        print("Human Score =  " + str(human_score))
+        difference = human_score - computer_score
+        print("The scores difference is " + str(difference))
 
-        roll_number = roll()
-        turn_total = turn_total + roll_number
-        print(roll_number, turn_total)
+        print("Would you like to roll?", end='' )
+        user_choice = input()
+        if user_choice in roll_again:
 
+            roll_number = roll()
+            turn_total = turn_total + roll_number
+            print(roll_number, turn_total)
+        else:
+            finished = True
+
+def main():
+    # Global variables
+    computer_score = 0
+    human_score = 0
+
+    human_move(computer_score, human_score)
+
+main()
