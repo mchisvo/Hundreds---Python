daf1b54dc3134228a38e6a25e76ab018938ab0e3
Michael Lampard <mlampa01@dcs.bbk.ac.uk>
2017-12-10 00:33:54 +0000

    Added turn_toal to print the users total for turn

--- hundreds.py
+++ hundreds.py
@@ -3,7 +3,7 @@ import random
 # Global variables
 computer_score = 0
 human_score = 0
-
+turn_total = 0
 
 def roll():
     """A function to generate the random number when a dice is rolled"""
@@ -19,14 +19,19 @@ def roll():
 while roll != 1:
     # Constants
     roll_again = ('y', 'Y')
+    # Turn variables
+
 
     print("Computers Score =  " + str(computer_score) + "\n")
     print("Human Score =  " + str(human_score))
     difference = human_score - computer_score
     print("The scores difference is " + str(difference))
 
-    print("Would you like to roll?", end='' + "\n")
+    print("Would you like to roll?", end='' )
     user_choice = input()
+    if user_choice in roll_again:
 
+        roll_number = roll()
+        turn_total = turn_total + roll_number
+        print(roll_number, turn_total)
 
-    print(roll())
