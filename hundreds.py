d33315d1eeccea9ee5c0441a97a98346957ea611
Michael Lampard <mlampa01@dcs.bbk.ac.uk>
2017-12-23 18:49:38 +0000

    Finished computer move and is_game_over, need to update computer risk logic to be a function

--- hundreds.py
+++ hundreds.py
@@ -51,8 +51,34 @@ def human_move(computer_score, human_score):
             breaker()
             finished = True
     human_score = human_score + move_total
+    game_status(computer_score, human_score)
     return human_score
 
+
+def computer_move(computer_score,human_score):
+    #game_status(computer_score, human_score) # instead use the human scored to detrmine difference and then risk
+    move_total = 0  # Initialize turn total variable for function
+    # Ad risk logic. i.e compyer decides to only try 4 rolls instead of 10 when its ahead
+    roll_limit = random.randint(1, 3)
+    die_roll_count = 0
+    finished = False
+    while not finished:
+        if die_roll_count != roll_limit:
+            roll_number = roll()
+            print("The computer rolled a: " + str(roll_number))
+            if roll_number == 1:
+                finished = True
+                move_total = 0
+            else:
+                move_total = move_total + roll_number
+                die_roll_count += 1
+        else:
+            print("Computer scored " + str(move_total) + " this move")
+            breaker()
+            finished = True
+
+    computer_score = computer_score + move_total
+    return computer_score
 
 def roll():
     """A function to generate the random number when a dice is rolled"""
@@ -60,16 +86,31 @@ def roll():
     roll = random.randint(1, 6)
     return roll
 
+
+def is_game_over(computer_score, human_score):
+    if human_score >= 50 or computer_score >= 50 and computer_score != human_score:
+        return True
+    else:
+        return False
+
+
 def main():
     # main function variables
     computer_score = 0
     human_score = 0
+    finished = False
+    while not finished:
 
-    #computer_score = computer_move(computer_score, human_score)
+        computer_score = computer_move(computer_score, human_score)
+        if computer_score >= 50:
+            human_score = human_move(computer_score, human_score)
+            finished = True
+        else:
+            human_score = human_move(computer_score, human_score)
+            finished = is_game_over(computer_score, human_score)
 
-    human_score = human_move(computer_score, human_score)
 
-    print(computer_score, human_score) # How do you bring the function variables out
+    print(computer_score, human_score)
 
 
 
