b04dd3500a17f6c730d4f793c9a8b6064a90129e
Michael Lampard <mlampa01@dcs.bbk.ac.uk>
2017-12-23 17:57:01 +0000

    Finished human move function

--- hundreds.py
+++ hundreds.py
@@ -3,8 +3,9 @@ import random
 
 def game_status(computer_score, human_score):
     """Function to print game status"""
+    breaker()
     print("Computers Score =  " + str(computer_score) + "\n")
-    print("Human Score =  " + str(human_score))
+    print("Human Score =  " + str(human_score) + "\n")
     difference = human_score - computer_score
     print("The scores difference is " + str(difference)+ "\n")
     breaker()
@@ -30,10 +31,28 @@ def ask_yes_or_no(prompt):
 
 
 def human_move(computer_score, human_score):
+    finished = False
+    move_total = 0
     game_status(computer_score, human_score)
+    while not finished:
+        if ask_yes_or_no(prompt = input("Would you like to roll?: ")):
+            roll_number = roll()
+            print('You rolled a: '+ str(roll_number))
+            if roll_number == 1:
+                move_total = 0
+                print("You scored " + str(move_total) + " this move")
+                breaker()
+                finished = True
+            else:
+                move_total = move_total + roll_number
 
-    if ask_yes_or_no(prompt = input("Would you like to roll?:")):
-        print(roll())
+        else:
+            print("You scored " + str(move_total) + " this move")
+            breaker()
+            finished = True
+    human_score = human_score + move_total
+    return human_score
+
 
 def roll():
     """A function to generate the random number when a dice is rolled"""
